76b26846bc0cb8944049355815b42f57a34e220f
Carolina Castro <caritobel1223@gmail.com>
2023-01-06 15:58:38 +0000

    mi primer commit

---
+++ src/js/component/card.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Card = function() {
+    return(<div className="card-group">
+    <div className="card">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-43qZispchLfKv4MgS-1Be5bKO1Q9KWjMU0A71wZdOg&s" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div className="card">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-43qZispchLfKv4MgS-1Be5bKO1Q9KWjMU0A71wZdOg&s" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div className="card">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-43qZispchLfKv4MgS-1Be5bKO1Q9KWjMU0A71wZdOg&s" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div className="card">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-43qZispchLfKv4MgS-1Be5bKO1Q9KWjMU0A71wZdOg&s" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>)
+}
+
+export default Card; 
